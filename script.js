c6953a8ee940cdb91a1f2470d3851bf997e08295
Danielchampion5 <danielchampionoficial@gmail.com>
2024-05-14 17:21:07 -0300

    feat: slides banner

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ let menu = document.getElementById("menu")
 let iconebarras = document.getElementById("icone-barras")
 let iconex = document.getElementById("icone-x")
 
-function abrirFecharMenu() 
-{
-    if (menu.classList.contains("menu-fechado")) 
-    {
+function abrirFecharMenu() {
+    if (menu.classList.contains("menu-fechado")) {
         menu.classList.remove("menu-fechado")
         iconex.style.display = "inline"
         iconebarras.style.display = "none"
     }
 
-    else 
-    {
+    else {
         menu.classList.add("menu-fechado")
 
         iconex.style.display = "none"
@@ -25,4 +22,51 @@ window.onresize = () => {
     menu.classList.remove("menu-fechado")
     iconex.style.display = "inline"
     iconebarras.style.display = "none"
+}
+
+let slides = [
+    'primeiro-banner',
+    'segundo-banner',
+    'terceiro-banner'
+]
+
+let slideAtual = 0
+
+let numeroSlides = slides.length
+
+let banner = document.querySelector(".banner")
+
+banner.classList.add(slides[slideAtual])
+
+function mostrarProximoSlide() 
+{
+
+    banner.classList.remove(slides[slideAtual])
+
+    if((slideAtual < numeroSlides - 1)) {
+        slideAtual++
+    }
+    else {
+        slideAtual = 0
+    }
+
+    banner.classList.add(slides[slideAtual])
+
+}
+
+function mostrarSlideAnterior() 
+{
+
+    banner.classList.remove(slides[slideAtual])
+
+    if(slideAtual>0) {
+        slideAtual--
+    }
+    else {
+        slideAtual = numeroSlides - 1
+    }
+
+
+    banner.classList.add(slides[slideAtual])
+
 }
